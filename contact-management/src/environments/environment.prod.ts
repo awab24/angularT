6b9e3862b6e2ff8507b7fa4f5f297c8cb7d4e782
Awab Hassan <awab1hassan1@gmail.com>
2024-08-04 23:25:14 +0300

    third commit

---
+++ contact-management/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+    production: true,
+    apiUrl: 'https://angular-pxb3v7tm3-awabs-projects-c15994b9.vercel.app/' // Example configuration
+  };
+  
